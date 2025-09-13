4dcf26944b180f403766eb8fcb1619ad89619488
duhamel <elie.duhamel@grenoble-inp.org>
2025-09-14 01:12:44 +0200

    test grid

--- src/simulation/grid.rs
+++ src/simulation/grid.rs
@@ -4,15 +4,15 @@ use std::sync::{Arc, Mutex};
 struct Grid{
 
 
-    data: HashMap<(i32, i32),Arc<Cell>>,
-    size_cell: (usize, usize),
+    data: HashMap<(i32, i32),Arc<Mutex<Cell>>>,
+    size_cell: (i32, i32),
 }
 
 
 struct Cell{
     coordonnee: (i32, i32),
-    taille: (usize, usize),
-    donne: [Cases],
+    taille: (i32, i32),
+    donne: Vec<Cases>,
 }
 struct Cases{
     pression : f64,
@@ -20,24 +20,43 @@ struct Cases{
     temperature : f64,
 }
 impl Grid{
-    fn get_cases(&self,coordinate: (i32, i32)) -> Option<&Cases>{
-        let cood_cell=(coordinate.0 / self.size_cell.0 as i32 , coordinate.1 / self.size_cell.1 as i32 );
-        match self.data.get(&cood_cell){
-            Some(cell)=>{
-                let cood_cases=(coordinate.0 % self.size_cell.0 as i32 , coordinate.1 % self.size_cell.1 as i32 );
-                cell.get_cases(cood_cases)
-            }
-            None=>{
-                return None;
-            }
+    pub fn where_case_try(&self, case_coordonne:(i32,i32))->Option<(i32,i32)>{
+        let coordonne = self.where_case(case_coordonne);
+        if self.data.contains_key(&coordonne) {
+            Some(coordonne)
+        } else {
+            None
         }
+    }
+    #[inline]
+    pub fn where_case(&self, case_coordonne:(i32,i32))->(i32,i32){
+        (case_coordonne.0/self.size_cell.0,case_coordonne.1/self.size_cell.1)
+    }
+    pub fn who_cells_by_cases(&self, case_coordonne_left_up:(i32,i32),case_coordonne_right_drown:(i32,i32))->Vec<Arc<Mutex<Cell>>>{
+        let mut cells = Vec::new();
+        //TODO
+
 
     }
 }
 impl Cell{
     fn get_cases(&self,coordinate: (i32, i32)) -> Option<&Cases>{
-        if coordinate.0>= 0 && coordinate.0 <self.taille.0 as i32 && coordinate.1 >= 0 && coordinate.1 < self.taille.1 as i32{
+        if coordinate.0>= self.taille.0*self.coordonnee.0 &&
+            coordinate.0 <self.taille.0*(self.coordonnee.0+1) &&
+            coordinate.1 >= self.taille.1*self.coordonnee.1 &&
+            coordinate.1 < self.taille.1*(self.coordonnee.1+1){
             Some(&self.donne[(coordinate.0+coordinate.1*self.taille.0 as i32 )as usize])
+        }else {
+            None
+        }
+
+    }
+    fn get_cases_mut(&mut self,coordinate: (i32, i32)) -> Option<&mut Cases>{
+        if coordinate.0>= self.taille.0*self.coordonnee.0 &&
+            coordinate.0 <self.taille.0*(self.coordonnee.0+1) &&
+            coordinate.1 >= self.taille.1*self.coordonnee.1 &&
+            coordinate.1 < self.taille.1*(self.coordonnee.1+1){
+            Some(&mut self.donne[(coordinate.0 + coordinate.1 * self.taille.0 as i32) as usize])
         }else {
             None
         }
